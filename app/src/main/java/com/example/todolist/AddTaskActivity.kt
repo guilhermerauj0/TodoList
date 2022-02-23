abeba03b98bdaa7cf717108efe80c0da6e2cc639
arthu <arthurguilhermesb@gmail.com>
2022-02-23 16:36:46 -0300

    create date picker and time picker

---
+++ app/src/main/java/com/example/todolist/AddTaskActivity.kt
@@ -0,0 +1,53 @@
+package com.example.todolist
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.todolist.databinding.ActivityAddTaskBinding
+import com.example.todolist.extensions.format
+import com.example.todolist.extensions.text
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.util.*
+
+class AddTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddTaskBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.tilDate.editText?.setOnClickListener {
+            val datepicker = MaterialDatePicker.Builder.datePicker().build()
+            datepicker.addOnPositiveButtonClickListener {
+                val timeZone = TimeZone.getDefault()
+                val offset = timeZone.getOffset(Date().time) * -1
+                binding.tilDate.text = Date(it + offset).format()
+
+            }
+            datepicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+
+        }
+
+        binding.tilHour.editText?.setOnClickListener {
+            val timePicker = MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .build()
+            timePicker.addOnPositiveButtonClickListener {
+                binding.tilHour.text = "${timePicker.hour}:${timePicker.minute}"
+            }
+            timePicker.show(supportFragmentManager, null)
+        }
+
+        binding.btnNewTask.setOnClickListener {
+
+        }
+    }
+}
--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var mainFab : FloatingActionButton = findViewById(R.id.main_fab)
+
+        mainFab.setOnClickListener {
+            val intent = Intent(this, AddTaskActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
